ab02cefee6f9d08d97c88fd53417b24f13931a46
Gianmarco Chistama <gianmarcovalentinc@gmail.com>
2024-12-12 13:31:17 -0500

    design/routes: añadir routing entre páginas

--- src/pages/CreateProduct.tsx
+++ src/pages/CreateProduct.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const CreateProduct = () => {
-	return <div>CreateProduct</div>;
+	return (
+		<>
+			<header className='flex justify-between'>
+				<h2 className='text-2xl text-slate-700'>Crear Producto</h2>
+				<Link
+					to='/'
+					className='bg-teal-700 p-3 text-sm font-bold text-white hover:bg-teal-600 rounded'>
+					Ver Productos
+				</Link>
+			</header>
+		</>
+	);
 };
 
 export default CreateProduct;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
-	return <div>Products</div>;
+	return (
+		<>
+			<header className='flex justify-between'>
+				<h2 className='text-2xl text-slate-700'>Lista de Productos</h2>
+				<Link
+					to='/crear'
+					className='bg-teal-700 p-3 text-sm font-bold text-white hover:bg-teal-600 rounded'>
+					Crear Producto
+				</Link>
+			</header>
+		</>
+	);
 };
 
 export default Products;
