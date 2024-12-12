1366f7177a1bf3b9fca01059bdc91c075fa5b4ed
Gianmarco Chistama <gianmarcovalentinc@gmail.com>
2024-12-12 13:26:04 -0500

    design: añadir diseño al layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,10 +2,14 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
 	return (
 		<>
-			<div>
-				<h1>Desde Layout</h1>
-			</div>
-			<Outlet />
+			<header className='bg-teal-900'>
+				<div className='container mx-auto py-10'>
+					<h1 className='text-3xl font-bold text-white text-center'>Administrador de Productos</h1>
+				</div>
+			</header>
+			<main className='container mx-auto mt-10 p-10 bg-white shadow'>
+				<Outlet />
+			</main>
 		</>
 	);
 };
